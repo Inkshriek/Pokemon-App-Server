4787d7b88568c50dd71a622be3502148b6fc8cab
Inkshriek <inkshriek@gmail.com>
2022-07-21 20:05:58 -0400

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const App = Express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://inkshriek.github.io/Pokemon-App');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
